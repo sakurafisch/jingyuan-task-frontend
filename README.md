2f16eeb10994483550c72ebd705a13182029892d
DangoSky <1341514988@qq.com>
2020-08-29 18:20:03 +0800

    feat: init
    Change-Id: I6068fac175ffeb9c58fca84ad70a8b3ff560307f

--- README.md
+++ README.md
@@ -4,5 +4,11 @@
 
 从零配置一套 React 项目脚手架，避免每次新开项目都只能使用 create-react-app。
 
-## 已配置功能
+## 配置功能
 
+- [] React 相关依赖
+- [] TypeScript 编译
+- [] Less 编译
+- [] Babel 编译
+- [] 打包压缩
+- [] 热重载
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react';
+
+class App extends PureComponent {
+  render() {
+    return (
+      <div>
+        <h1>Hello React</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
