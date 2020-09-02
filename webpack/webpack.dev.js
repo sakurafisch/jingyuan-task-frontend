c7a7ad8c62527852c33c10214e6da13634eae0f8
DangoSky <1341514988@qq.com>
2020-09-02 15:15:44 +0800

    feat: source map
    Change-Id: Iea17f19c6830ff6f0f8198d358bd335b3f58a1ec

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,9 +1,14 @@
+const webpack = require('webpack');
 const { merge } = require('webpack-merge');
 const commonConfig = require('./webpack.common.js');
 const path = require('path');
 
 const devConfig = merge(commonConfig, {
   mode: 'development',
+  devtool: "cheap-module-eval-source-map",
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
