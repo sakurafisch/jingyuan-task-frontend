cc3e248495eecbac8a43c1fd463dce29f64f290e
DangoSky <1341514988@qq.com>
2020-09-04 13:19:30 +0800

    feat: delete analyze plugin
    Change-Id: I5d8269b87273bdae3574828999e2bbc51a5ce777

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,8 +1,6 @@
 const webpack = require('webpack');
 const { merge } = require('webpack-merge');
 const path = require('path');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-
 const commonConfig = require('./webpack.common.js');
 
 const devConfig = merge(commonConfig, {
@@ -10,7 +8,6 @@ const devConfig = merge(commonConfig, {
   devtool: "cheap-module-eval-source-map",
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new BundleAnalyzerPlugin(),
   ],
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
