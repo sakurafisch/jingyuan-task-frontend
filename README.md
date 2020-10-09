6abeb04510bf8eb4627e638c5ed9084ffa61726e
winnerwinter1998 <winnerwinter1998@gmail.com>
2020-10-09 16:32:13 +0800

    在TableView添加了选择数据库和选择日期的功能

--- README.md
+++ README.md
@@ -10,28 +10,46 @@
 
 ### 2020年9月24日
 
-使用模板构建`React`环境，整理项目配置
+使用模板构建 `React` 环境，整理项目配置
 
 ### 2020年9月25日
 
-开发`login`模块及其子模块
+开发 `login` 模块及其子模块
 
 ### 2020年10月8日
+
+引入 `echarts` ，用于做柱状图和折线图
 
 ```bash
 yarn
 yarn add echarts --save
 ```
 
-删除`package-lock.json`以避免使用`npm`
+删除 `package-lock.json` 以避免使用 `npm`
 
-做了一下首页的`SideBar`
+做了一下首页的 `SideBar`
 
-### 2020年10月9日
+### 2020年10月9日早上
 
 整理了一下首页的代码
 
 登录功能勉强能用
+
+### 2020年10月9日下午
+
+引入 `ali-react-table` ，用于做表格
+
+```bash
+yarn add ali-react-table
+```
+
+清理 `echarts` ，因为有更好的候选方案。
+
+```bash
+yarn remove echarts
+```
+
+之后可能会用 [antd-chart](https://charts.ant.design/demos/column/#%E5%9F%BA%E7%A1%80%E6%9F%B1%E7%8A%B6%E5%9B%BE) 来绘制柱状图和折线图。
 
 ## 模板
 
--- src/pages/chart/tableview/index.tsx
+++ src/pages/chart/tableview/index.tsx
@@ -1,11 +1,60 @@
 import React, { Component } from 'react'
+import { DatePicker } from 'antd';
+import { Select } from 'antd';
+import { autobind } from 'core-decorators';
+import { ArtColumn, BaseTable } from 'ali-react-table';
 
+const { Option } = Select;
+
+function onDateChange(date: any, dateString: string) {
+  console.log(date, dateString);
+}
+
+function onSelectChange(value: any) {
+    console.log(`selected ${value}`);
+}
+
+@autobind
 export default class Tableview extends Component {
     render() {
         return (
             <div>
-                表格
-            </div>
+            <Select
+                showSearch
+                style={{ width: 200 }}
+                placeholder="Select Area"
+                optionFilterProp="children"
+                onChange={onSelectChange}
+            >
+                <Option value="V10000">碧桂园</Option>
+                <Option value="V10001">翰博三期</Option>
+                <Option value="V10002">翰博二期</Option>
+                <Option value="V10003">丽景</Option>
+                <Option value="V10004">宏颐</Option>
+                <Option value="V10005">龙腾</Option>
+                <Option value="V10006">府梓</Option>
+                <Option value="V10007">绿茵</Option>
+                <Option value="V10008">人杰二期</Option>
+                <Option value="V10009">碧桂园高层</Option>
+                <Option value="V10010">碧桂园南区</Option>
+                <Option value="V10011">碧桂园北区</Option>
+                <Option value="V10012">碧桂园别墅区</Option>
+                <Option value="V10013">明发</Option>
+                <Option value="V10014">太湖一期</Option>
+                <Option value="V10015">太湖二期</Option>
+                <Option value="V10016">人杰一期</Option>
+                <Option value="V10017">亚太换一期</Option>
+                <Option value="V10018">亚太换二期</Option>
+                <Option value="V10019">碧桂园西区</Option>
+                <Option value="V10020">北苑</Option>
+                <Option value="V10021">亚泰三期</Option>
+                <Option value="V10022">翰博皇家御湾</Option>
+                <Option value="V10023">太湖明珠</Option>
+                <Option value="V10024">碧桂园东北区</Option>
+                <Option value="V10025">博物馆</Option>
+            </Select>
+            <DatePicker onChange={onDateChange} />
+          </div>
         )
     }
 }
